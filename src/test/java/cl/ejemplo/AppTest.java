2f0ef3afb1e5788e1bc7ac3efae6af5d6feed21e
Julio Cesar Azocar Mansilla <jazocarm@poderjudicial.cl>
2026-08-10 15:39:52 -0400

    feat(app): hola mundo java con test y github actions

---
+++ src/test/java/cl/ejemplo/AppTest.java
@@ -0,0 +1,12 @@
+package cl.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void deberia_devolver_hola_mundo() {
+        assertEquals("Hola Mundo", App.saludo());
+    }
+}
